7b17d3b72a8d051309b8c0ff5bf0bdbd1fc2ae57
G. MULLER <gm@presans.com>
2017-02-19 22:35:48 +0100

    SpeedUp process by 1) removing output 2) looping on auxiliary trainSet and adding/rbinding by batch instead of line by line + Added col+rownames to aloow correct rbinding + Writing correct unix-like CSV + adding debug

--- newcode/TrainSet.R
+++ newcode/TrainSet.R
@@ -1,18 +1,57 @@
 source("Utils.R");
 
+EMPTY_VECTOR <- rep(0, length(CLASSES))
+names(EMPTY_VECTOR) <- paste("c", CLASSES, sep="");
+
+## rbinds numSet to wholeSet, keeping colnames from numSet and
+## reassigning fresh rownames and returns the result
+mergeSets <- function(wholeSet, numSet) {
+    ###print("--- rownames(wholeSet$data)") ; print(rownames(wholeSet$data));
+    ###print("--- colnames(wholeSet$data)");  print(colnames(wholeSet$data));
+    ###print("--- rownames(numSet$data)");    print(rownames(numSet$data));
+    ###print("--- colnames(numSet$data)");    print(colnames(numSet$data));
+    newData    <- rbind(wholeSet$data, numSet$data);
+    colnames(newData) <- colnames(numSet$data);
+    rownames(newData) <- 1:nrow(newData);
+    ###print("+++ rownames(wholeSet$classes)");    print(rownames(wholeSet$classes));
+    ###print("+++ colnames(wholeSet$classes)");    print(colnames(wholeSet$classes));
+    ###print("+++ rownames(numSet$classes)");      print(rownames(numSet$classes));
+    ###print("+++ colnames(numSet$classes)");      print(colnames(numSet$classes));
+    newClasses <- rbind(wholeSet$classes,numSet$classes);
+    colnames(newClasses) <- colnames(numSet$classes);
+    rownames(newClasses) <- 1:nrow(newClasses);
+    return(list(data=newData, classes=newClasses));
+}
+
 ## Store image in TrainSet
 addToTrainSet <- function(trainSet, img, trueClass) {
     ## Rescale to very small image
     scaledImg <- resize(img, w=TRAIN_WIDTH, h=TRAIN_HEIGHT); ## auto keep-ratio? / filter="none"/"bilinear"?
     ## "flatten" image (2D df -> 1D vector)
     flatImg <- c(scaledImg, recursive=T);
+    names(flatImg) <- paste("i", 1:(TRAIN_WIDTH*TRAIN_HEIGHT), sep="");
+      ###print(">>>addToTrainSet::names(flatImg)");      print(names(flatImg));
     ## Add Class info
-    class <- rep(0, length(CLASSES))
-    names(class) <- CLASSES
-    class[trueClass] <- 1  # vec are 1-based
-    return(list(data=rbind(trainSet$data,flatImg), classes=rbind(trainSet$classes,class)));
+    class <- EMPTY_VECTOR
+    class[p("c",trueClass)] <- 1
+      ###print(">>>addToTrainSet::names(class)");      print(names(class));
+
+    newData    <- rbind(trainSet$data, flatImg);
+    colnames(newData) <- names(flatImg);
+    rownames(newData) <- 1:nrow(newData);
+      ###print("***addToTrainSet::rownames(newData)");      print(rownames(newData));
+      ###print("***addToTrainSet::colnames(newData)");      print(colnames(newData));
+    newClasses <- rbind(trainSet$classes, class);
+    colnames(newClasses) <- names(class);
+    rownames(newClasses) <- 1:nrow(newClasses);
+      ###print("***addToTrainSet::rownames(newClasses)");      print(rownames(newClasses));
+      ###print("***addToTrainSet::colnames(newClasses)");      print(colnames(newClasses));
+    return(list(data=newData, classes=newClasses));
 }
 
+##  Based on 0-9+E+H original images, use
+##  tilt+rotation+transl+blur+resize, generates 12 * .8/.2 * 60/15 *
+##  20/5 * 20/5 * 2/.5 * .4/.1 =49152 images to learn on.
 generateTrainSet <- function() {
     trainSet <- list(data=data.frame(), classes=data.frame())
     for (cl in CLASSES) {
@@ -20,6 +59,9 @@ generateTrainSet <- function() {
         origImg <- readImage(p(INDIR_IMAGES,cl,".png"));
         imgSize <- dim(origImg);
 
+        print(paste(date(), "Treating Class: ", cl));
+
+        trainSetAux <- list(data=data.frame(), classes=data.frame())  ## to split in smallest set => speedup?
         for(tilt in seq(from=-.4,to=.4,by=.2)) {
             ##for(tilt in seq(from=-.4,to=.4,length.out=5)) {
             mconvol <- matrix(c(1,tilt,-tilt*imgSize[[1]],0,1,0), ncol=2)
@@ -39,26 +81,30 @@ generateTrainSet <- function() {
                                 blured   <- gblur(transtd, sigma);
                                 resized  <- resize(blured, w=ratio*imgSize[[1]]);
 
-                                newFile <- p(OUTDIR_IMAGES, cl,
-                                             "_t", tilt,
-                                             "_r", angle,
-                                             "_t", x,"_", y,
-                                             "_b", sigma,
-                                             "_s", ratio, ".png");
+                                ## newFile <- p(OUTDIR_IMAGES, cl,
+                                ## "_t", tilt,
+                                ## "_r", angle,
+                                ## "_t", x,"_", y,
+                                ## "_b", sigma,
+                                ## "_s", ratio, ".png");
 
                                 ## To debug/speed up next iterations
                                 ##print(paste("Writing file: ", newFile));
                                 ##writeImage(resized, newFile);
 
-                                print(paste("Treating file: ", newFile));
-                                trainSet <- addToTrainSet(trainSet, resized, cl);
-                                print(paste("New trainSet size: ", object.size(trainSet)));
+                                ##print(paste("Treating file: ", newFile));
+                                trainSetAux <- addToTrainSet(trainSetAux, resized, cl);
+                                ##print(trainSetAux);
+                                ##print(paste("New trainSetAux size: ", object.size(trainSetAux)));
                             } # size
                         } # blur
                     } # translate y
                 } # translate x
             } # rotate
-        } # titlt
+        } # tilt
+        trainSet <- mergeSets(trainSet,trainSetAux);
+        rm(trainSetAux); # cleans up memory
+        print(paste("New trainSet size: ", object.size(trainSet)));
     } # classes/"numbers"
 
     return(trainSet);
@@ -81,22 +127,22 @@ showImg <- function(trainSet, row) {
 
 # saves "trainSet" to Data+Classes files starting with "prefix"
 saveTrainSetToCSV <- function(trainSet, prefix) {
-    write.csv2(x=trainSet$data, p(prefix,"Data.csv"), sep=",");       # TODO: check
-    write.csv2(x=trainSet$classes, p(prefix,"Classes.csv"), sep=","); # TODO: check
+    write.csv(x=trainSet$data, p(prefix,"Data.csv"));
+    write.csv(x=trainSet$classes, p(prefix,"Classes.csv"));
 }
 
 # reloads "trainSet" from Data+Classes files starting with "prefix"
 loadTrainSetFromCSV <- function(prefix) {
-    trainSet <- list(); # TODO: check
-    trainSet$data    <- read.csv2(file=p(prefix,"Data.csv"), sep=",");    # TODO: check
-    trainSet$classes <- read.csv2(file=p(prefix,"Classes.csv"), sep=","); # TODO: check
+    trainSet <- list();
+    trainSet$data    <- read.csv(file=p(prefix,"Data.csv"));
+    trainSet$classes <- read.csv(file=p(prefix,"Classes.csv"));
     return(trainSet);
 }
 
 ## Re-arrange trainSet by concatenating Data+Classes, particularly for NeuralNet lib
 mergeDataAndClasses <- function(trainSet) {
     trainSetPlusClass <- cbind(trainSet$data, trainSet$classes);
-    inames <- paste("i", 1:TRAIN_WIDTH*TRAIN_HEIGHT, sep="");
+    inames <- paste("i", 1:(TRAIN_WIDTH*TRAIN_HEIGHT), sep="");
     cnames <- paste("c", CLASSES, sep="");
     colnames(trainSetPlusClass) <- c(inames,cnames);
     return(trainSetPlusClass);
@@ -125,6 +171,7 @@ main <- function() {
     filename <- p(OUTDIR_TRAINSET,"trainSet");
     trainSet <- generateTrainSet();
     saveTrainSetToCSV(trainSet, filename);
-    trainSet2 <- loadTrainSetFromCSV(filename);
-    trainSetPlusClass <- mergeDataAndClasses(trainSet); ## TODO: check
+    #trainSet2 <- loadTrainSetFromCSV(filename);
+    trainSetPlusClass <- mergeDataAndClasses(trainSet);
+    write.csv(trainSetPlusClass, p(filename,"Whole.csv"));
 }
