71a355b4c7bdb6edbfabc703ceea7d0ea5ae7e1f
G. Muller <guillaumemuller75421354@gmail.com>
2017-03-14 00:26:44 +0100

    Adding scripts to get trainsets directly from Mega.nz

---
+++ trainsets/get_trainsets.sh
@@ -0,0 +1,5 @@
+
+mkdir -p full/ splitted/
+megacopy -u 'TempSharing@yopmail.com' -p 'Temp?Sharing!' --download --local=full/ --remote=/Root/RedDigitsTrainSets/full/
+megacopy -u 'TempSharing@yopmail.com' -p 'Temp?Sharing!' --download --local=splitted/ --remote=/Root/RedDigitsTrainSets/splitted/
+
