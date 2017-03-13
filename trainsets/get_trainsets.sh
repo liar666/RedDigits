0eaa9df8b08d2b97d35b5fd6ab543b1b3469b402
G. MULLER <gm@presans.com>
2017-03-13 23:47:31 +0100

    Adding script to get trainsets from mega

---
+++ trainsets/get_trainsets.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+mkdir -p full splitted
+megacopy -u 'TempSharing@yopmail.com' -p 'Temp?Sharing!' --download --local=full/ --remote=/Root/RedDigitsTrainSets/full/
+megacopy -u 'TempSharing@yopmail.com' -p 'Temp?Sharing!' --download --local=splitted/ --remote=/Root/RedDigitsTrainSets/splitted/
