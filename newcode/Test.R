7e342ff36714c34ef34db92f2cdc6efe94636d95
G. MULLER <gm@presans.com>
2017-03-30 23:39:11 +0200

    Adding code to test an image

---
+++ newcode/Test.R
@@ -0,0 +1,63 @@
+## source("../code/pick.R");
+## img<-readImage("../images/numbers_orig/5.png");
+## display(img);
+## img<-pickColor(img,c(1.0,0.0,0.0),c(.2,.2,.3));
+## img2<-channel(img,"red");
+## display(img2);
+## img2<-img2>.5;
+## img2 <- sharpenImage(img2,.3);
+## display(img2);
+## img3<-resize(img2, 10, 14);
+## display(img3)
+## flatImg <- c(img3, recursive=T);
+## flatDF <- as.data.frame(t(flatImg));
+## names(flatDF) <- colnames(test[,dataCols])
+## preds <- nn.predict(fitDN3, flatDF);
+## names(test)[140+max.col(preds)];
+
+
+source("../code/pick.R");
+
+FOREGROUND <- c(1.0,0.0,0.0);
+FOREGROUND_MARGINS <- c(0.3,0.2,0.2);
+FOREGROUND_THRESHOLD <- .85;
+
+
+### Erodes the borders
+sharpenImage <- function(image, force) {
+    kern <- makeBrush(force, shape='disc'); # force was 5 in docs
+    return(erode(image, kern));
+}
+
+preprocess <- function(img) {
+    img <- sharpenImage(img, 2);
+    if(colorMode(img)==2) {
+        redImg  <- pickColor(img, FOREGROUND, FOREGROUND_MARGINS);
+        redChan <- channel(redImg,"red") > FOREGROUND_THRESHOLD;
+        return(redChan);
+    }
+    return(img);
+}
+five  <- readImage("../images/numbers_orig/5.png");
+## Sharpen first, then preprocess (DEACTIVATE sharpening inside preprocess)
+five2 <- sharpenImage(five, 3);
+five2 <- preprocess(five2);
+display(five2);
+## Preprocess then sharpen (DEACTIVATE sharpening inside preprocess)
+five3 <- preprocess(five);
+five4 <- sharpenImage(five3, 3);
+display(five4);
+
+predictImage <- function(img, model, predFunc) {
+    img2 <- preprocess(img); ## sharp+reduce colors+seuil=>crisp BW...
+    scaledImg <- resize(img2, w=TRAIN_WIDTH, h=TRAIN_HEIGHT); ## auto keep-ratio? / filter="none"/"bilinear"?
+    flatImg <- c(scaledImg, recursive=T);
+    names(flatImg) <- COL_IMG_NAMES;
+    predFunc(model, flatImg); ### flatImg[dataCols]
+}
+predictImage(five, fitDN3, nn.predict);
+
+predictFile <- function(filename, predFun) {
+    origImg <- readImage(filename);
+    return(predictImage(origImag));
+}
