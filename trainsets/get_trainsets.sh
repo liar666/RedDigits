ba331ba25799e0e69cc9b22d3688ad57ef5ac921
Guigui <guigui@stuart.little>
2018-12-09 08:44:32 +0100

    removing useless dirs

--- trainsets/get_trainsets.sh
+++ trainsets/get_trainsets.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 
-mkdir -p full/ splitted/
-
 megacopy -u 'TempSharing@yopmail.com' -p 'Temp?Sharing!' --download --local=BlackAndWhite/ --remote=/Root/RedDigitsTrainSets/BlackAndWhite/
 megacopy -u 'TempSharing@yopmail.com' -p 'Temp?Sharing!' --download --local=BlackAndRed/ --remote=/Root/RedDigitsTrainSets/BlackAndRed/
