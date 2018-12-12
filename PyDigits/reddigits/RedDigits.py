bd8c8c4833e6a07c3b2b970bcc3fe5d2d4a086ac
Guillaume MULLER <guillaumemuller75421354@gmail.com>
2018-12-12 17:25:31 +0100

    main() gets args from commanline + added more verbosity

--- PyDigits/reddigits/RedDigits.py
+++ PyDigits/reddigits/RedDigits.py
@@ -7,37 +7,43 @@ First attempt at building a induction cooking plate's digits detector in python
 
 import sys    # To get commanline arguments
 
+import pandas as pd
+
+import matplotlib.pyplot as plt
+
+#from sklearn import __version__                   # To keep track of current SkLearn version for unloading model
+
 #import DigitModel as dimo
 import DigitPositionDetector as dipo
 import DigitValueDetector as dival
 
-from sklearn import __version__                   # To keep track of current SkLearn version for unloading model
 
-
-
-
-def main(argv):
-    if (len(argv)!=3):
-        print("USAGE: " + argv[0] + " <image file> <model file>")
+if __name__ == "__main__":
+    if (len(sys.argv)!=3):
+        print("USAGE: " + sys.argv[0] + " <image file> <model file>")
         sys.exit(1)
     else:
-        imageComplete = argv[1] ## dipo.DigitPositionDetector.DETECT_DIR + "/example_3.png"
+        #imageComplete = dipo.DigitPositionDetector.DETECT_DIR + "/example_3.png"
+        imageComplete = sys.argv[1]
         posDect = dipo.DigitPositionDetector(imageComplete)
         posDect.detect()
+        posDect.displayImage()
         digits = posDect.getDetectedDigits()
-    
+
         valDect = dival.DigitValueDetector()
         solver = 'lbfgs'
-        hidden = (100, 50)
-        modelFilename = argv[2] ## valDect.MODELS_DIR + "/sklearn_" + __version__ + "_mlp_" + solver + "_" + str(hidden) + ".joblib"
+        hidden = (10, 5)
+        #modelFilename = valDect.MODELS_DIR + "/sklearn_" + __version__ + "_mlp_" + solver + "_" + str(hidden) + ".joblib" # TODO argv[2]
+        modelFilename = sys.argv[2]
         valDect.loadModel(modelFilename);
 
         for digit in digits:
-             digit.guessedValue = valDect.convertProbas2Class(valDect.testSingleInstance(valDect.imageToFeatures(digit.subImage)))
-             print("Seen a "+ digit.guessedValue + " at " + digit.fuzzyPosition)
+            digitAsFeatures = pd.DataFrame(valDect.imageToFeatures(digit.subImage))
+            digit.guessedValue = valDect.convertProbas2Class(valDect.testSingleInstance(digitAsFeatures))
+            digit.display()
+            print("Seen a "+ str(digit.guessedValue) + " at " + digit.fuzzyPosition + " " + str(digit.center))
 
-if __name__ == "__main__":
-    main(sys.argv)
+
 
 ## TODOs:
 ## 1. Preprocessing1
